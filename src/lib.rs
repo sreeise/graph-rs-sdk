0e8511d5143114ff911574cba64c5b31f45c5be8
sreeise <reeisesean@gmail.com>
2019-03-10 18:59:15 -0400

    Change pub use to pub extern crate for re-exporting dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
 #![feature(associated_type_defaults)]
-extern crate strum;
+pub extern crate strum;
 #[macro_use]
-extern crate strum_macros;
+pub extern crate strum_macros;
 #[macro_use]
-extern crate serde_derive;
+pub extern crate serde_derive;
+pub extern crate serde_json;
+pub extern crate serde;
+pub extern crate reqwest;
+pub extern crate jsonfile;
+pub extern crate base64;
+pub extern crate graph_error;
+pub extern crate graph_oauth;
+pub extern crate encoding;
 
 pub mod drive;
 pub mod process;
-pub use drive::*;
