f0c285c11b025595a0dcdbda3d36e3685d8e7350
sreeise <reeisesean@gmail.com>
2019-08-24 14:14:12 -0400

    API method impls

--- src/drive/item.rs
+++ src/drive/item.rs
@@ -573,11 +573,11 @@ pub trait ItemMe: MutateRequest + AsMut<DriveUrl> + AsMut<DataPipeline> {
 pub trait ItemCommon:
     MutateRequest + AsMut<DriveUrl> + AsMut<DataPipeline> + Into<SelectEvent>
 {
-    fn get_drive(&mut self, resource_id: &str) -> Request<DriveInfo> {
+    fn drive(&mut self, resource_id: &str) -> Request<DriveInfo> {
         self.extend_path(&[resource_id, "drives"]);
         Request::from(Pipeline::new(self.pipeline_data(), DriveEvent::Delete))
     }
-    
+
     fn delete(&mut self, item_id: &str, resource_id: &str) -> Request<StatusResponse> {
         self.format_common(item_id, resource_id);
         self.as_delete();
