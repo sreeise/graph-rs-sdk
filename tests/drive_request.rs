e36a5712077d12754279610514cef5b7836d403e
sreeise <reeisesean@gmail.com>
2019-10-03 05:09:39 -0400

    Add separate env variables

--- tests/drive_request.rs
+++ tests/drive_request.rs
@@ -7,39 +7,25 @@ use test_tools::ci::CI;
 
 #[test]
 fn drive_tests_on_ci() {
-    if CI::is_travis() {
-        let token = CI::request_access_token().unwrap();
-        let t = token.bearer_token().clone();
-        let id = env::var("TEST_APP_USER_ID").expect("Missing env USER_ID");
-        get_recent(t, id.as_str());
-        create_delete_folder(t, id.as_str());
-    } else {
-        CI::assert_not_travis();
+    if let Some(token) = CI::request_access_token() {
+        let t = token.1.bearer_token().clone();
+        get_drive(t, token.0.as_str());
+        get_recent(t, token.0.as_str());
+        create_delete_folder(t, token.0.as_str());
     }
 }
 
 fn get_recent(token: &str, rid: &str) {
     let client = Graph::new(token);
-    if let Ok(response) = client.v1().drives(rid).drive().recent().send() {
-        assert!(response.value().len() > 0)
-    } else {
+    if let Err(_) = client.v1().drives(rid).drive().recent().send() {
         panic!("Request Error. Method: drive recent");
     }
 }
 
-#[test]
-fn get_drive() {
-    if CI::is_travis() {
-        CI::test_credentials(|t| {
-            if let Some((bearer, id)) = t {
-                let client = Graph::new(bearer.as_str());
-                if let Err(_) = client.v1().drives(id.as_str()).drive().root().send() {
-                    panic!("Request Error. Method: drive root");
-                }
-            }
-        });
-    } else {
-        CI::assert_not_travis();
+fn get_drive(token: &str, rid: &str) {
+    let client = Graph::new(token);
+    if let Err(_) = client.v1().drives(rid).drive().root().send() {
+        panic!("Request Error. Method: drive root");
     }
 }
 
@@ -78,35 +64,31 @@ fn create_delete_folder(token: &str, rid: &str) {
 
 #[test]
 fn root_children_and_list_versions() {
-    if CI::is_travis() {
-        CI::test_credentials(|t| {
-            if let Some((bearer, id)) = t {
-                let client = Graph::new(bearer.as_str());
-                if let Ok(res) = client
+    CI::test_credentials(|t| {
+        if let Some((id, bearer)) = t {
+            let client = Graph::new(bearer.as_str());
+            if let Ok(res) = client
+                .v1()
+                .drives(id.as_str())
+                .drive()
+                .root_children()
+                .send()
+            {
+                let value = res.value().index(0).clone().unwrap();
+                let item_id = value.id.clone().unwrap();
+
+                if let Err(_) = client
                     .v1()
                     .drives(id.as_str())
                     .drive()
-                    .root_children()
+                    .list_versions(item_id.as_str())
                     .send()
                 {
-                    let value = res.value().index(0).clone().unwrap();
-                    let item_id = value.id.clone().unwrap();
-
-                    if let Err(_) = client
-                        .v1()
-                        .drives(id.as_str())
-                        .drive()
-                        .list_versions(item_id.as_str())
-                        .send()
-                    {
-                        panic!("Request Error. Method: list versions");
-                    }
-                } else {
-                    panic!("Request Error. Method: drive root children");
+                    panic!("Request Error. Method: list versions");
                 }
+            } else {
+                panic!("Request Error. Method: drive root children");
             }
-        });
-    } else {
-        CI::assert_not_travis();
-    }
+        }
+    });
 }
