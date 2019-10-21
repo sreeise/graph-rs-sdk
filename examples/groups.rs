4ade77bbf7c8178fe115d37f691b1bf2811ea74d
sreeise <reeisesean@gmail.com>
2019-10-21 19:58:36 -0400

    Update groups get method to not include extra id. Closes #240. Update groups method naming.

---
+++ examples/groups.rs
@@ -0,0 +1,189 @@
+use graph_rs::prelude::*;
+use graph_error::GraphResult;
+
+static ACCESS_TOKEN: &str = "<ACCESS_TOKEN>";
+
+static GROUP_ID: &str = "<GROUP_ID>";
+
+fn main() {}
+
+#[allow(dead_code)]
+fn get_or_list_groups() -> GraphResult<()> {
+    let client = Graph::new(ACCESS_TOKEN);
+
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .get()
+        .send()?;
+
+    println!("Group response: {:#?}", response);
+
+    let response = client.v1()
+        .groups("") // Won't be used
+        .list()
+        .send()?;
+
+    println!("List groups response: {:#?}", response);
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn add_favorites_members_owners() -> GraphResult<()> {
+    let client = Graph::new(ACCESS_TOKEN);
+
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .add_favorite()
+        .send()?;
+
+    println!("Add favorite response: {:#?}", response);
+
+    let directory_object_id = "<DIRECTORY_OBJECT_ID>";
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .add_member(&serde_json::json!({
+            "@odata.id": format!("https://graph.microsoft.com/v1.0/directoryObjects/{}", directory_object_id)
+        }))
+        .send()?;
+
+    println!("Add member response: {:#?}", response);
+
+    let user_id = "<USER_ID>";
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .add_owner(&serde_json::json!({
+            "@odata.id": format!("https://graph.microsoft.com/v1.0/users/{}", user_id)
+        }))
+        .send()?;
+
+    println!("Add owner response: {:#?}", response);
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn create_update_delete_group() -> GraphResult<()> {
+    let client = Graph::new(ACCESS_TOKEN);
+
+    let response = client.v1()
+        .groups("") // Won't be used.
+        .create(&serde_json::json!({
+            "description": "Self help community for library",
+            "displayName": "Library Assist",
+            "groupTypes": [
+                "Unified"
+            ],
+            "mailEnabled": true,
+            "mailNickname": "library",
+            "securityEnabled": false
+        }))
+        .send()?;
+
+    println!("Create group: {:#?}", response);
+
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .update(&serde_json::json!({
+            "description": "description-value",
+            "displayName": "displayName-value",
+            "groupTypes": [
+                "groupTypes-value"
+            ],
+            "mail": "mail-value",
+            "mailEnabled": true,
+            "mailNickname": "mailNickname-value"
+            }
+        ))
+        .send()?;
+
+    println!("Update group: {:#?}", response);
+
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .delete()
+        .send()?;
+
+    println!("Delete group: {:#?}", response);
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn list_methods() -> GraphResult<()> {
+    let client = Graph::new(ACCESS_TOKEN);
+
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .list_members()
+        .send()?;
+
+    println!("{:#?}", response);
+
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .list_member_of()
+        .send()?;
+
+    println!("{:#?}", response);
+
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .list_transitive_members()
+        .send()?;
+
+    println!("{:#?}", response);
+
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .list_transitive_member_of()
+        .send()?;
+
+    println!("{:#?}", response);
+
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .list_owners()
+        .send()?;
+
+    println!("{:#?}", response);
+
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .list_photos()
+        .send()?;
+
+    println!("{:#?}", response);
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn remove_methods() -> GraphResult<()> {
+    let client = Graph::new(ACCESS_TOKEN);
+
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .remove_favorite()
+        .send()?;
+
+    println!("{:#?}", response);
+
+    let member_id = "<MEMBER_ID>";
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .remove_member(member_id)
+        .send()?;
+
+    println!("{:#?}", response);
+
+    let owner_id = "<OWNER_ID>";
+    let response = client.v1()
+        .groups(GROUP_ID)
+        .remove_owner(owner_id)
+        .send()?;
+
+    println!("{:#?}", response);
+
+    Ok(())
+}
