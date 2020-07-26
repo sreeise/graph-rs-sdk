d80caa24c026427d1c28e74cf07487e29362b7c3
sreeise <reeisesean@gmail.com>
2020-07-26 13:47:14 -0400

    Fix Travis CI environment variables

--- tests/secure_env.rs
+++ tests/secure_env.rs
@@ -3,8 +3,19 @@ use test_tools::oauthrequest::OAuthRequest;
 // Make sure secure environment variables are being decrypted correctly
 // on Travis CI.
 #[test]
-fn test_decrypted_env() {
+fn travis_ci_env_variable() {
     if OAuthRequest::is_travis() {
+        let (id, token) = OAuthRequest::request_access_token().unwrap();
+        assert!(!token.bearer_token().is_empty());
+        assert!(!id.is_empty());
+    }
+}
+
+// Make sure secure environment variables are being decrypted correctly
+// on Appveyor.
+#[test]
+fn appveyor_env_variable() {
+    if OAuthRequest::is_appveyor() {
         let (id, token) = OAuthRequest::request_access_token().unwrap();
         assert!(!token.bearer_token().is_empty());
         assert!(!id.is_empty());
