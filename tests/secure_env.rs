fa249ab7beeacfc178697b7bcee645cb690b7d1c
sreeise <reeisesean@gmail.com>
2020-07-29 05:30:39 -0400

    Change the way we test for being on ci environemnt

--- tests/secure_env.rs
+++ tests/secure_env.rs
@@ -5,9 +5,7 @@ use test_tools::oauthrequest::OAuthRequest;
 #[test]
 fn travis_ci_env_variable() {
     if OAuthRequest::is_travis() {
-        let (id, token) = OAuthRequest::request_access_token().unwrap();
-        assert!(!token.bearer_token().is_empty());
-        assert!(!id.is_empty());
+        OAuthRequest::is_test_env_set()
     }
 }
 
@@ -16,8 +14,6 @@ fn travis_ci_env_variable() {
 #[test]
 fn appveyor_env_variable() {
     if OAuthRequest::is_appveyor() {
-        let (id, token) = OAuthRequest::request_access_token().unwrap();
-        assert!(!token.bearer_token().is_empty());
-        assert!(!id.is_empty());
+        assert!(OAuthRequest::is_test_env_set());
     }
 }
