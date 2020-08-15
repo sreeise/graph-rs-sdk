4c96139680381fab5dbe50cb0daad51472458655
Sean Reeise <35837533+sreeise@users.noreply.github.com>
2020-08-15 13:44:39 -0400

    Disabled env checks in tests to prevent failures on PR's from forks

--- tests/secure_env.rs
+++ tests/secure_env.rs
@@ -4,16 +4,24 @@ use test_tools::oauthrequest::OAuthRequest;
 // on Travis CI.
 #[test]
 fn travis_ci_env_variable() {
+    // Uncomment if testing out new secure environment variables
+    // to make sure they get set correctly.
+    /*
     if OAuthRequest::is_travis() {
         assert!(OAuthRequest::is_test_env_set());
     }
+     */
 }
 
 // Make sure secure environment variables are being decrypted correctly
 // on Appveyor.
 #[test]
 fn appveyor_env_variable() {
+    // Uncomment if testing out new secure environment variables
+    // to make sure they get set correctly.
+    /*
     if OAuthRequest::is_appveyor() {
         assert!(OAuthRequest::is_test_env_set());
     }
+    */
 }
