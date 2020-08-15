f774c008b495ceb4984a0b87757f8c50236bf024
sreeise <reeisesean@gmail.com>
2020-08-15 04:42:33 -0400

    Clean up/shorten wellknown requests

--- graph-oauth/src/discovery/wellknown.rs
+++ graph-oauth/src/discovery/wellknown.rs
@@ -1,38 +1,26 @@
-use graph_error::GraphFailure;
+use graph_error::GraphResult;
 
 #[derive(Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
 pub struct WellKnown;
 
 impl WellKnown {
-    pub fn signing_keys<T>(url: &str) -> Result<T, GraphFailure>
+    pub fn signing_keys<T>(url: &str) -> GraphResult<T>
     where
         for<'de> T: serde::Deserialize<'de>,
     {
-        let client = reqwest::blocking::Client::builder().build()?;
-        let response = client.get(url).send();
-
-        match response {
-            Ok(t) => {
-                let keys: T = t.json()?;
-                Ok(keys)
-            },
-            Err(e) => Err(GraphFailure::from(e)),
-        }
+        let client = reqwest::blocking::Client::new();
+        let response = client.get(url).send()?;
+        let keys: T = response.json()?;
+        Ok(keys)
     }
 
-    pub async fn async_signing_keys<T>(url: &str) -> Result<T, GraphFailure>
+    pub async fn async_signing_keys<T>(url: &str) -> GraphResult<T>
     where
         for<'de> T: serde::Deserialize<'de>,
     {
         let client = reqwest::Client::new();
-        let response = client.get(url).send().await;
-
-        match response {
-            Ok(t) => {
-                let keys: T = t.json().await?;
-                Ok(keys)
-            },
-            Err(e) => Err(GraphFailure::from(e)),
-        }
+        let response = client.get(url).send().await?;
+        let keys: T = response.json().await?;
+        Ok(keys)
     }
 }
