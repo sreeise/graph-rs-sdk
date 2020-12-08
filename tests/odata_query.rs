230ee5edf1add50be0d3a8b1d282dfce08e5bfe7
sreeise <reeisesean@gmail.com>
2020-12-08 04:43:16 -0500

    Fix odata query methods and add tests

---
+++ tests/odata_query.rs
@@ -0,0 +1,48 @@
+use graph_rs::prelude::*;
+use test_tools::assert_url_eq;
+
+#[test]
+fn select_query() {
+    let client = Graph::new("token");
+
+    client.v1().me().drive().get_drive().select(&["id", "name"]);
+    assert_url_eq(&client, "/me/drive?select=id%2Cname");
+}
+
+#[test]
+fn expand_query() {
+    let client = Graph::new("token");
+
+    client.v1().me().drive().get_drive().expand(&["users"]);
+    assert_url_eq(&client, "/me/drive?expand=users");
+}
+
+#[test]
+fn filter_query() {
+    let client = Graph::new("token");
+
+    client
+        .v1()
+        .me()
+        .drive()
+        .get_drive()
+        .filter(&["startsWith(displayName,'j')"]);
+    assert_url_eq(
+        &client,
+        "/me/drive?filter=startsWith%28displayName%2C%27j%27%29",
+    );
+}
+
+#[test]
+fn expand_filter_query() {
+    let client = Graph::new("token");
+
+    client
+        .v1()
+        .me()
+        .drive()
+        .get_drive()
+        .expand(&["users"])
+        .filter(&["name"]);
+    assert_url_eq(&client, "/me/drive?expand=users&filter=name");
+}
