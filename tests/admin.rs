e88170b3610805a8dd718b2bd1bcc1ef1fbeffc3
Sean Reeise <35837533+sreeise@users.noreply.github.com>
2023-03-10 18:16:36 -0500

    Update tests

--- tests/admin.rs
+++ tests/admin.rs
@@ -1,4 +1,7 @@
 use graph_core::resource::ResourceIdentity;
+use graph_http::api_impl::ODataQuery;
+use graph_http::ResponseHandler;
+use graph_rs_sdk::prelude::*;
 use test_tools::common::TestTools;
 use test_tools::oauthrequest::*;
 
@@ -6,15 +9,14 @@ use test_tools::oauthrequest::*;
 async fn list_health_issues_test() {
     if let Some((_id, client)) = OAuthTestClient::graph_by_rid_async(ResourceIdentity::Admin).await
     {
-        let result = client
-            .v1()
+        let response = client
             .admin()
-            .service_announcement()
             .list_issues()
             .filter(&["startDateTime ge 2022-02-22 and startDateTime lt 2022-02-23"])
-            .text()
-            .await;
+            .send()
+            .await
+            .unwrap();
 
-        TestTools::assert_success(&result, "service_announcements.list_issues by text");
+        assert!(response.status().is_success());
     }
 }
--- tests/app_catalogs_url.rs
+++ tests/app_catalogs_url.rs
@@ -1,44 +1,47 @@
 #[macro_use]
 extern crate lazy_static;
 
-use graph_http::BlockingHttpClient;
 use graph_rs_sdk::prelude::*;
-use test_tools::assert_url_eq;
 use test_tools::common::TestTools;
 
 lazy_static! {
     static ref ID_VEC: Vec<String> = TestTools::random_strings(2, 20);
 }
 
-pub fn graph() -> Graph<BlockingHttpClient> {
-    Graph::new("")
+pub fn graph() -> GraphV2 {
+    GraphV2::new("")
 }
 
 #[test]
 fn app_catalogs_urls() {
     let client = graph();
 
-    client.v1().app_catalogs().list_teams_apps();
-    assert_url_eq(&client, "/appCatalogs/teamsApps");
+    assert_eq!(
+        "/v1.0/appCatalogs/teamsApps".to_string(),
+        client.app_catalogs().list_teams_apps().url().path()
+    );
 
-    client
-        .v1()
-        .app_catalogs()
-        .get_teams_apps(ID_VEC[0].as_str());
-    assert_url_eq(&client, &format!("/appCatalogs/teamsApps/{}", ID_VEC[0]));
+    assert_eq!(
+        format!("/v1.0/appCatalogs/teamsApps/{}", ID_VEC[0]),
+        client
+            .app_catalogs()
+            .get_teams_apps(ID_VEC[0].as_str())
+            .url()
+            .path()
+    );
 }
 
 #[test]
 fn team_apps_urls() {
     let client = graph();
-    client
-        .v1()
-        .app_catalogs()
-        .teams_apps()
-        .list_app_definitions(ID_VEC[0].as_str());
-    assert_url_eq(
-        &client,
-        &format!("/appCatalogs/teamsApps/{}/appDefinitions", ID_VEC[0]),
+
+    assert_eq!(
+        format!("/v1.0/appCatalogs/teamsApps/{}/appDefinitions", ID_VEC[0]),
+        client
+            .app_catalogs()
+            .list_app_definitions(ID_VEC[0].as_str())
+            .url()
+            .path()
     );
 }
 
@@ -46,18 +49,15 @@ fn team_apps_urls() {
 fn app_definition_urls() {
     let client = graph();
 
-    client
-        .v1()
-        .app_catalogs()
-        .teams_apps()
-        .app_definitions()
-        .get_bot(ID_VEC[0].as_str(), ID_VEC[1].as_str());
-
-    assert_url_eq(
-        &client,
-        &format!(
-            "/appCatalogs/teamsApps/{}/appDefinitions/{}/bot",
+    assert_eq!(
+        format!(
+            "/v1.0/appCatalogs/teamsApps/{}/appDefinitions/{}/bot",
             ID_VEC[0], ID_VEC[1]
         ),
+        client
+            .app_catalogs()
+            .get_bot(ID_VEC[0].as_str(), ID_VEC[1].as_str())
+            .url()
+            .path()
     );
 }
