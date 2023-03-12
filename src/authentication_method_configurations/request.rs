de33317281419d55b7899b418a5d5e49c7d5f639
Sean Reeise <35837533+sreeise@users.noreply.github.com>
2023-03-12 04:36:35 -0400

    Update authentication methods configuration and policy api clients

--- src/authentication_method_configurations/request.rs
+++ src/authentication_method_configurations/request.rs
@@ -1,61 +1,47 @@
 // GENERATED CODE
 
 use crate::api_default_imports::*;
-use graph_http::types::NoContent;
 
-register_client!(AuthenticationMethodConfigurationsRequest,);
-register_client!(AuthenticationMethodConfigurationsIdRequest, ());
+resource_api_client!(
+    AuthenticationMethodConfigurationsApiClient,
+    AuthenticationMethodConfigurationsIdApiClient,
+    ResourceIdentity::AuthenticationMethodConfigurations
+);
 
-impl<'a, Client> AuthenticationMethodConfigurationsRequest<'a, Client>
-where
-    Client: graph_http::RequestClient,
-{
-    post!({
+impl AuthenticationMethodConfigurationsApiClient {
+    post!(
         doc: "Add new entity to authenticationMethodConfigurations",
         name: create_authentication_method_configuration,
-        response: serde_json::Value,
         path: "/authenticationMethodConfigurations",
-        has_body: true
-    });
-    get!({
+        body: true
+    );
+    get!(
         doc: "Get entities from authenticationMethodConfigurations",
         name: list_authentication_method_configuration,
-        response: serde_json::Value,
-        path: "/authenticationMethodConfigurations",
-        has_body: false
-    });
-    get!({
+        path: "/authenticationMethodConfigurations"
+    );
+    get!(
         doc: "Get the number of the resource",
-        name: get_authentication_methods_configurations_count,
-        response: serde_json::Value,
-        path: "/authenticationMethodConfigurations/$count",
-        has_body: false
-    });
+        name: get_authentication_method_configurations_count,
+        path: "/authenticationMethodConfigurations/$count"
+    );
 }
 
-impl<'a, Client> AuthenticationMethodConfigurationsIdRequest<'a, Client>
-where
-    Client: graph_http::RequestClient,
-{
-    delete!({
+impl AuthenticationMethodConfigurationsIdApiClient {
+    delete!(
         doc: "Delete entity from authenticationMethodConfigurations",
         name: delete_authentication_method_configuration,
-        response: NoContent,
-        path: "/authenticationMethodConfigurations/{{RID}}",
-        has_body: false
-    });
-    get!({
+        path: "/authenticationMethodConfigurations/{{RID}}"
+    );
+    get!(
         doc: "Get entity from authenticationMethodConfigurations by key",
         name: get_authentication_method_configuration,
-        response: serde_json::Value,
-        path: "/authenticationMethodConfigurations/{{RID}}",
-        has_body: false
-    });
-    patch!({
+        path: "/authenticationMethodConfigurations/{{RID}}"
+    );
+    patch!(
         doc: "Update entity in authenticationMethodConfigurations",
         name: update_authentication_method_configuration,
-        response: NoContent,
         path: "/authenticationMethodConfigurations/{{RID}}",
-        has_body: true
-    });
+        body: true
+    );
 }
