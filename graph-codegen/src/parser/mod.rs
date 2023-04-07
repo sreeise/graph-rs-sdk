555f3bff09ad02c62e35b7c848633e2bee5db3f1
Sean Reeise <35837533+sreeise@users.noreply.github.com>
2023-04-07 03:30:52 -0400

    Remove unused codegen

--- graph-codegen/src/parser/mod.rs
+++ graph-codegen/src/parser/mod.rs
@@ -1,10 +1,6 @@
-mod directory_mod;
 pub mod error;
-mod parser_settings;
 mod request;
 mod resource;
 
-pub use directory_mod::*;
-pub use parser_settings::*;
 pub use request::*;
 pub use resource::*;
--- graph-codegen/src/settings/mod.rs
+++ graph-codegen/src/settings/mod.rs
@@ -1,11 +1,7 @@
 mod api_client_link;
-mod custom_requests;
 mod method_macro_modifier;
-mod operation_modifier;
 mod resource_settings;
 
 pub use api_client_link::*;
-pub use custom_requests::*;
 pub use method_macro_modifier::*;
-pub use operation_modifier::*;
 pub use resource_settings::*;
