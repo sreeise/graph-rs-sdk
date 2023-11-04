68611496c8a4d130000c93c9aee5ecdedd838d32
Jacek Kurlit <jacek.kurlit@kitopi.com>
2023-11-04 17:56:02 +0100

    code format

--- src/drives/worksheets_used_range/request.rs
+++ src/drives/worksheets_used_range/request.rs
@@ -2,18 +2,21 @@
 
 use crate::api_default_imports::*;
 
-resource_api_client!(WorksheetsUsedRangeApiClient, ResourceIdentity::WorksheetsUsedRange);
+resource_api_client!(
+    WorksheetsUsedRangeApiClient,
+    ResourceIdentity::WorksheetsUsedRange
+);
 
 impl WorksheetsUsedRangeApiClient {
-	get!(
-		doc: "Invoke function usedRange", 
-		name: workbook_worksheet,
-		path: "/usedRange()"
-	);
-	get!(
-		doc: "Invoke function usedRange", 
-		name: workbook_worksheet,
-		path: "/usedRange(valuesOnly={{id}})",
-		params: values_only
-	);
+    get!(
+        doc: "Invoke function usedRange",
+        name: workbook_worksheet,
+        path: "/usedRange()"
+    );
+    get!(
+        doc: "Invoke function usedRange",
+        name: workbook_worksheet,
+        path: "/usedRange(valuesOnly={{id}})",
+        params: values_only
+    );
 }
