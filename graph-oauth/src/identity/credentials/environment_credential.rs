dba0f0e4430861f5885ef7c0c7569d1b76f3b328
Jack Eadie <jack.eadie0@gmail.com>
2024-09-02 09:44:12 +1000

    Make `EnvironmentCredential::username_password_env` public

--- graph-oauth/src/identity/credentials/environment_credential.rs
+++ graph-oauth/src/identity/credentials/environment_credential.rs
@@ -105,7 +105,7 @@ impl EnvironmentCredential {
         ))
     }
 
-    fn username_password_env(
+    pub fn username_password_env(
         tenant_id: Option<String>,
         azure_client_id: String,
         azure_username: String,
